fff039160dec6e5d0d4f3f31d225fd884a479183
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2023-11-15 18:27:22 +0200

    create style file & add title to home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/core/utils/styles.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +9,21 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
         FeaturedBooksListView(),
+        SizedBox(
+          height: 50,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        )
       ],
     );
   }
 }
-
