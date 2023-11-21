e854f3ef3f90becf15102393890cf82c9a868d19
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2023-11-21 15:55:56 +0200

    add either to home repo method & create failure calss

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:book_app/core/errors/failures.dart';
+import 'package:book_app/features/home/data/models/book_model/book_model.dart';
+// ignore: depend_on_referenced_packages
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeatueredBooks();
+  Future<Either<Failure ,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure ,List<BookModel>>> fetchFeatueredBooks();
 }
