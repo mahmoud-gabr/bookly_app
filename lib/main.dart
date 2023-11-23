8d022334ef961e5c95d35a01373729df6e0282fa
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2023-11-23 19:12:01 +0200

    add get it to our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:book_app/constants.dart';
+import 'package:book_app/core/utils/api_service.dart';
 import 'package:book_app/core/utils/app_routers.dart';
+import 'package:book_app/core/utils/service_locator.dart';
+import 'package:book_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:book_app/features/home/presentation/manger/feature_books_cubit/featured_books_cubit.dart';
+import 'package:book_app/features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-
 
 void main() {
   runApp(const BooklyApp());
@@ -14,13 +20,28 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        routerConfig: AppRouters.router,
       ),
-      routerConfig: AppRouters.router,
     );
   }
 }
