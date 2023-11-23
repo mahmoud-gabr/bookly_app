28d6e3adb58eea6af60d7dfd129c6fcf4c020f66
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2023-11-23 18:33:59 +0200

    create newest books cubit

--- lib/features/home/presentation/manger/feature_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/feature_books_cubit/featured_books_cubit.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:book_app/features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
-
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
--- lib/features/home/presentation/manger/feature_books_cubit/featured_books_state.dart
+++ lib/features/home/presentation/manger/feature_books_cubit/featured_books_state.dart
@@ -13,12 +13,10 @@ final class FeaturedBooksLoading extends FeaturedBooksState {}
 
 final class FeaturedBooksFailure extends FeaturedBooksState {
   final String errMessage;
-
   const FeaturedBooksFailure({required this.errMessage});
 }
 
 final class FeaturedBooksSucsess extends FeaturedBooksState {
   final List<BookModel> books;
-
   const FeaturedBooksSucsess({required this.books});
 }
