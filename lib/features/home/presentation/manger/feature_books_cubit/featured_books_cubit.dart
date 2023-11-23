a9b79ccb264297a6f2d80a7905dc2ae007265430
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2023-11-23 18:33:20 +0200

    create newest books cubit (states & method)

--- lib/features/home/presentation/manger/feature_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/feature_books_cubit/featured_books_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:book_app/core/errors/failures.dart';
 import 'package:book_app/features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:book_app/constants.dart';
 import 'package:book_app/core/utils/app_routers.dart';
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
+
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 void main() {
   runApp(const BooklyApp());
